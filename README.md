2cfa2dc1cae11031d6030fadcf61bc1eac88dcf0
= <=>
2020-11-12 21:53:13 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# @digibear/middleware
+
+This is a middleware engine written in typescript that I've used on a couple of projects now. I've decided to turn it into it's own module for future reuse.
+
+## Installation
+
+`yarn add @udigibear/middleware`
+
+## Usage
+
+When you call the pipline function and assign it to a variable, you are able to pass a coma seperated list of middleware functions to add to the middleware queue.
+
+```JS
+import pipeline from "@digibear/middleware";
+
+interface Context {
+    [key: string]: any
+}
+
+const engine = pipeline<Context>((ctx, next) => {
+    ctx.foobar="baz";
+    next();
+})
+```
+
+Not all middleware has to be added when the factory function is first invoked. Instead, you can assign middldeware to the system later with the `use` method.
+
+```js
+engine.use((ctx, next) => {
+  ctx.another = 123;
+  next();
+});
+```
+
+Then when we're ready to run the middleware engine, we'll create it's it's initial context, and then pass that context to the engine for processing.
+
+```js
+(async () => {
+  const context: Context = {};
+  await engine.exec(context);
+  console.log(context);
+})();
+
+// => { foobar: "baz", another: 123 }
+```
+
+## Development
+
+`yarn -Dev install`
+
+## Testing
+
+`yarn test`
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+// First we declare our types.
+export type Next = () => Promise<any> | any;
+export type Middleware<T> = (
+  context: T,
+  next: Next
+) => Promise<void | T> | void | T;
+export type Pipe<T> = {
+  use: (...middlewares: Middleware<T>[]) => void;
+  execute: (context: T) => Promise<void | T>;
+};
+
+/**
+ * Declare a new middleware pipeline.
+ * @param middlewares A list of middlewares to add to the pipeline on 
+ * instantiation.
+ */
+export default function <T>(...middlewares: Middleware<T>[]): Pipe<T> {
+  const stack: Middleware<T>[] = middlewares;
+
+  /**
+   * Add middlewares to the pipeline.
+   * @param middlewares A list of middlewares to add to the current
+   * pipeline.
+   */
+  const use: Pipe<T>["use"] = (...middlewares) => {
+    stack.push(...middlewares);
+  };
+
+  /**
+   * Execute a pipeline, and move context through each middleware in turn.
+   * @param context The contect object to be sent through the pipeline.
+   */
+  const execute: Pipe<T>["execute"] = async (context) => {
+    let prevIndex = -1;
+
+    /**
+     * Programatically go through each middleware and apply it to context before
+     * either moving on to the next middleware, or returning the final context
+     * object.
+     * @param index The current count of middleware executions.
+     * @param context The context object to send through the
+     * middleware pipeline.
+     */
+    const handler = async (index: number, context: T): Promise<void | T> => {
+      if (index === prevIndex) {
+        throw new Error("next() already called.");
+      }
+
+      if (index === stack.length) return context;
+
+      prevIndex = index;
+
+      const middleware = stack[index];
+
+      if (middleware) {
+        await middleware(context, () => handler(index + 1, context));
+      }
+    };
+    const response = await handler(0, context);
+    if (response) return response;
+  };
+
+  return { use, execute };
+}
